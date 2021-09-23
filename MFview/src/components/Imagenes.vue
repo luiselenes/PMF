f7dd194cf63496f4c87526062f4194ca76e91cb4
luiselenes <luiseelenes@outlook.con>
2021-09-22 19:17:08 -0600

    js_imagen

--- MFview/src/components/Imagenes.vue
+++ MFview/src/components/Imagenes.vue
@@ -1,7 +1,23 @@
 <template>
     <div class="Imagenes">
         <form action="">
-            <input type="file" name='route[image_route]'/>
+            <input type="file" name= 'route[image_route]'/>
+            <input type="file" @change= "onFileSelected"> 
         </form>
     </div>
 </template>
+
+<script>
+export default {
+    name:'Image',
+    data (){
+        return{
+        }
+    },
+    methods:{
+        onFileSelected(event){
+        console.log(event)
+        }
+   }
+}
+</script>
